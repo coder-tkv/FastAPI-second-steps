a4461734149a64e8c6fa4fdd2324f047bf197353
tkv <egahaker@mail.ru>
2025-04-26 23:31:33 +0300

    added OOP inheritance

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ from pydantic import BaseModel, Field, EmailStr
 
 data = {
     'email': 'abc@mail.ru',
-    'bio': 'Я вкусный пирожок',
+    'bio': 'Я пирожок',
     'age': 12
+}
+
+data_wo_age = {
+    'email': 'abc@mail.ru',
+    'bio': 'Я пирожок'
 }
 
 
 class UserSchema(BaseModel):
     email: EmailStr
     bio: str | None = Field(max_length=10)
+
+
+class UserAgeSchema(UserSchema):
     age: int = Field(ge=0, le=130)
 
 
-user = UserSchema(**data)
+user = UserAgeSchema(**data)
+user_wo_age = UserSchema(**data_wo_age)
 print(repr(user))
+print(repr(user_wo_age))
