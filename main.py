f8022d5e1b2b2289fca1bd8375733e0c0121f073
tkv <egahaker@mail.ru>
2025-05-08 23:28:37 +0300

    added files

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
-import time
-import asyncio
 import uvicorn
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse, FileResponse
 
 app = FastAPI()
 
 
-def sync_task():
-    time.sleep(3)
-    print('Сделан запрос в сторонний API')
-
-
-async def async_task():
-    await asyncio.sleep(3)
-    print('Отправлен email')
-
-
-@app.post('/async')
-async def run_async():
-    ...
-    asyncio.create_task(async_task())
+@app.post('/files')
+async def upload_file(uploaded_file: UploadFile):
+    file = uploaded_file.file
+    filename = uploaded_file.filename
+    with open(filename, 'wb') as f:
+        f.write(file.read())
     return {'ok': True}
 
 
-@app.post('/sync')
-async def run_sync(bg_tasks: BackgroundTasks):
-    ...
-    bg_tasks.add_task(sync_task)
+@app.post('/multiple_files')
+async def upload_file(uploaded_files: list[UploadFile]):
+    for uploaded_file in uploaded_files:
+        file = uploaded_file.file
+        filename = uploaded_file.filename
+        with open(filename, 'wb') as f:
+            f.write(file.read())
     return {'ok': True}
+
+
+@app.get('/files/{filename}')
+async def get_file(filename: str):
+    return FileResponse(filename)
+
+
+def iterfile(filename: str):
+    with open(filename, 'rb') as f:
+        while chunk := f.read(1024 * 1024):
+            yield chunk
+
+@app.get('/files/streaming/{filename}')
+async def get_streaming_file(filename: str):
+    return StreamingResponse(iterfile(filename), media_type='video/mp4')
 
 
 if __name__ == '__main__':
